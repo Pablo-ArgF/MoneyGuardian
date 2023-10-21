57cf6a82d70721f4aaf3e698498971d4e62ec38a
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-10-21 14:51:49 +0200

    Cambiado el layout de Pagos Conjuntos para que sea un Fragment.

---
+++ app/src/main/java/com/moneyguardian/PagosConjuntosFragment.java
@@ -0,0 +1,55 @@
+package com.moneyguardian;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagosConjuntosFragment extends Fragment {
+
+    // Identificadores de intent
+
+    public static final String PAGO_CONJUNTO_SELECCIONADO = "pago_conjunto_seleccionado";
+    public static final String PAGO_CONJUNTO_CREADO = "pago_conjunto_creado";
+
+    // Identificador de activiy
+    // TODO Necesario?
+    public static final int GESTION_ACTIVITY = 1;
+
+    // Modelo de datos
+
+    // TODO private ArrayList<PagoConjunto> listaPagosConjuntos;
+    // TODO private PagoConjunto pagoConjunto;
+    private RecyclerView listaPagosConjuntosView;
+
+    public PagosConjuntosFragment() {
+        // Required empty public constructor
+    }
+
+    public static PagosConjuntosFragment newInstance(String param1, String param2) {
+        PagosConjuntosFragment fragment = new PagosConjuntosFragment();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+           // mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pagos_conjuntos, container, false);
+    }
+}
