80d469ff66d0f322c939e9997e1565f261354a91
UO283970 <98962779+UO283970@users.noreply.github.com>
2023-10-29 14:02:56 +0100

    Inicio del form para la creación de items de pago.

---
+++ app/src/main/java/com/moneyguardian/FormItemsListaPago.java
@@ -0,0 +1,14 @@
+package com.moneyguardian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormItemsListaPago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_items_lista_pago);
+    }
+}
