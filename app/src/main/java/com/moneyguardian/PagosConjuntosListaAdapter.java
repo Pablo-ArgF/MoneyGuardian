fce6268f8e288d418c40c7c17e55f52ec05be48d
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-11-09 21:02:05 +0100

    Ahora la imagen del pago se puede ver en la lista.

--- app/src/main/java/com/moneyguardian/PagosConjuntosListaAdapter.java
+++ app/src/main/java/com/moneyguardian/PagosConjuntosListaAdapter.java
@@ -1,8 +1,10 @@
 package com.moneyguardian;
 
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.moneyguardian.modelo.ItemPagoConjunto;
 import com.moneyguardian.modelo.PagoConjunto;
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -54,6 +57,7 @@ public class PagosConjuntosListaAdapter extends RecyclerView.Adapter<PagosConjun
         private TextView name;
         private TextView money;
         private TextView fecha;
+        private ImageView image;
 
         public PagosConjuntosListaViewHolder(View itemView) {
             super(itemView);
@@ -61,6 +65,7 @@ public class PagosConjuntosListaAdapter extends RecyclerView.Adapter<PagosConjun
             name = (TextView) itemView.findViewById(R.id.TextNombrePagoConjuntoLinea);
             money = (TextView) itemView.findViewById(R.id.editTextPagoConjuntoCoste);
             fecha = (TextView) itemView.findViewById(R.id.TextFechaPagoConjuntoLinea);
+            image = (ImageView) itemView.findViewById(R.id.imgViewPagoConjunto);
         }
 
         // asignar valores a los componentes
@@ -69,6 +74,10 @@ public class PagosConjuntosListaAdapter extends RecyclerView.Adapter<PagosConjun
             money.setText(calculatePrecio(pagoConjunto));
             String creacionPago = new SimpleDateFormat("dd-MM-yyyy").format(pagoConjunto.getFechaPago());
             fecha.setText(String.format(creacionPago));
+            // Colocamos la imagen si no es que hay
+            if (pagoConjunto.getImagen() != null) {
+                Picasso.get().load(pagoConjunto.getImagen()).into(image);
+            }
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
