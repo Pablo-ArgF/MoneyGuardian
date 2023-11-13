1f2e8ae3b2474d233630d2e42f18715476fea5d4
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-11-14 00:36:00 +0100

    Menu not working

--- app/src/main/java/com/moneyguardian/MainActivity.java
+++ app/src/main/java/com/moneyguardian/MainActivity.java
@@ -1,14 +1,14 @@
 package com.moneyguardian;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,7 +18,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.moneyguardian.ui.PagosConjuntosFragment;
 import com.moneyguardian.userAuth.LoginActivity;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -32,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private FirebaseFirestore db;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-
         FirebaseUser user = auth.getCurrentUser();
-        //TODO revisar porque al borrar la cuenta a veces hace login
         if(user == null){
             //if no user -> send to login page
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
@@ -81,7 +81,36 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
 
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.home_menu_btn) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        }
+        if(id == R.id.lista_menu_btn){
+            //TODO implement lista view
+            return false;
+        }
+        if(id == R.id.amigos_menu_btn){
+            startActivity(new Intent(this, SocialActivity.class));
+            return true;
+        }
+        if(id == R.id.pagos_conjuntos_menu_btn){
+            startActivity(new Intent(this,SocialActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
