2bff2e01567c3b5951ca3f25ff36a5fc0abb2153
UO283970 <98962779+UO283970@users.noreply.github.com>
2023-11-22 00:32:23 +0100

    Imagenes de usuarios

--- app/src/main/java/com/moneyguardian/FormItemsListaPago.java
+++ app/src/main/java/com/moneyguardian/FormItemsListaPago.java
@@ -35,14 +35,12 @@ import com.moneyguardian.modelo.ItemPagoConjunto;
 import com.moneyguardian.modelo.PagoConjunto;
 import com.moneyguardian.modelo.UsuarioParaParcelable;
 import com.moneyguardian.util.DecimalFilterForInput;
-import com.moneyguardian.UsersFormItemsListaAdapter;
 import com.moneyguardian.util.UsuarioMapper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 public class FormItemsListaPago extends AppCompatActivity {
 
