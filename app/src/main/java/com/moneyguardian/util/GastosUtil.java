ca85ee04ec422205ecab76dfd4389c7bda6b0edd
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-11-28 18:46:54 +0100

    Ahora al borrar un gasto, se borra también la referencia de la colección de usuarios.

--- app/src/main/java/com/moneyguardian/util/GastosUtil.java
+++ app/src/main/java/com/moneyguardian/util/GastosUtil.java
@@ -1,10 +1,14 @@
 package com.moneyguardian.util;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.moneyguardian.modelo.Gasto;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class GastosUtil {
@@ -19,6 +23,22 @@ public class GastosUtil {
     public static void deleteGasto(Gasto g) {
         if (g.getReference() != null) {
             g.getReference().delete();
+            // Borramos ese pago de la lista de pagos del usuario
+            db.collection("users/").document(
+                    auth.getUid().toString()).update("gastos", FieldValue.arrayRemove(g.getReference()));
+            /**userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override public void onSuccess(DocumentSnapshot documentSnapshot) {
+            ArrayList<DocumentReference> docs = (ArrayList<DocumentReference>) documentSnapshot.get("gastos");
+            // TODO concurrency error
+            for (DocumentReference dRef : docs) {
+            if (dRef.equals(g.getReference())) {
+            docs.remove(dRef);
+            }
+            }
+            userRef.update("gastos", docs);
+            }
+            });
+             **/
         }
     }
 
