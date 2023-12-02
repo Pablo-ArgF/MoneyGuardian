0b33438b65269aef1b7265cd7d7b2413f1fe4040
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-12-02 21:45:20 +0100

    Callejon sin salida para sacar los usuarios

--- app/src/main/java/com/moneyguardian/adapters/DeudaListaAdapter.java
+++ app/src/main/java/com/moneyguardian/adapters/DeudaListaAdapter.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.moneyguardian.R;
 import com.moneyguardian.modelo.ItemPagoConjunto;
 import com.moneyguardian.modelo.PagoConjunto;
@@ -17,6 +22,7 @@ import com.moneyguardian.modelo.dto.DeudaDTO;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +30,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class DeudaListaAdapter extends RecyclerView.Adapter<DeudaListaAdapter.DeudaViewHolder> {
+    private static FirebaseAuth auth = FirebaseAuth.getInstance();
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private List<DeudaDTO> deudas;
 
@@ -49,13 +57,35 @@ public class DeudaListaAdapter extends RecyclerView.Adapter<DeudaListaAdapter.De
         return deudas.size();
     }
 
+    // TODO no se como hacer que la db no vaya en el adapter...
     public void update(@NonNull List<PagoConjunto> pagosConjuntos, String userID) {
         for (PagoConjunto pago : pagosConjuntos) {
+            HashMap<UsuarioParaParcelable, Double> pagos = new HashMap<>();
             for (ItemPagoConjunto item : pago.getItems()) {
+                for (Map.Entry<UsuarioParaParcelable, Double> entry : item.getPagos().entrySet()) {
+                    // Obtener los datos de cada item desde la DB
+                    db.collection("/users").document(entry.getKey().getNombre()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot doc = task.getResult();
+                                String nombre = (String) doc.get("name");
+                                String email = (String) doc.get("email");
+                                String imagen = (String) doc.get("profilePicture");
+                                UsuarioParaParcelable user = new UsuarioParaParcelable(nombre, email, imagen, doc.getId());
+                                pagos.put(user, entry.getValue());
+                            }
+                        }
+                    });
+                }
+                while (pagos.size() != item.getPagos().size()) {
+                    // No me mates Pablo please
+                }
+                item.setPagos(pagos);
                 calculateDeuda(item.getPagos(), userID);
+                notifyDataSetChanged();
             }
         }
-        notifyDataSetChanged();
     }
 
     private void calculateDeuda(@NonNull Map<UsuarioParaParcelable, Double> mapItems, String userID) {
@@ -88,13 +118,13 @@ public class DeudaListaAdapter extends RecyclerView.Adapter<DeudaListaAdapter.De
             }
         }
         // Si el usuario es el pagador, necesitaremos hacer varias deudas
-        if (isUserPagador) {
+        if (isUserPagador && pagador != null && !usuario.isEmpty()) {
             for (UsuarioParaParcelable usuarioDebe : usuario) {
                 DeudaDTO deuda = new DeudaDTO(pagador, usuarioDebe, total);
                 deuda.setUserPagador(true);
                 this.deudas.add(deuda);
             }
-        } else {
+        } else if (pagador != null && mainUser != null) {
             DeudaDTO deuda = new DeudaDTO(pagador, mainUser, total);
             this.deudas.add(deuda);
         }
