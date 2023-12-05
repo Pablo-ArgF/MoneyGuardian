6a1f63625956a2fcc1b1c4aa5788ca57495761c0
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-12-05 20:26:32 +0100

    Arreglado un error que no dejaba volver a la pantalla.

--- app/src/main/java/com/moneyguardian/ListasGastosDuedasFragment.java
+++ app/src/main/java/com/moneyguardian/ListasGastosDuedasFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,11 +21,13 @@ public class ListasGastosDuedasFragment extends Fragment {
     private int currentFragment;
     private DeudasListFragment deudasListFragmentragment;
     private ListaGastosFragment listaGastosFragment;
+    private BottomNavigationView bottomNavigationView;
 
     public ListasGastosDuedasFragment() {
         // Required empty public constructor
         listaGastosFragment = new ListaGastosFragment();
         deudasListFragmentragment = new DeudasListFragment();
+        currentFragment = 0;
     }
 
     @Override
@@ -37,20 +40,23 @@ public class ListasGastosDuedasFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_listas_gastos_duedas, container, false);
 
-        getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, listaGastosFragment).commit();
-        currentFragment = 0;
-        BottomNavigationView bottomNavigationView = root.findViewById(R.id.listasNavView);
+        // TODO error, al volver al fragment no carga ninguno de los otros dos fragments
+        if (currentFragment == 0) {
+            getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, listaGastosFragment).commit();
+        }
+
+        bottomNavigationView = root.findViewById(R.id.listasNavView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.menu_deudas && R.id.menu_deudas != currentFragment) {
                     currentFragment = R.id.menu_deudas;
-                    getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, deudasListFragmentragment).commit();
+                    getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, deudasListFragmentragment).addToBackStack(null).commit();
                     return true;
                 } else if (item.getItemId() == R.id.menu_gastos && R.id.menu_gastos != currentFragment) {
                     ((MainActivity) getActivity()).setLoading(false);
                     currentFragment = R.id.menu_gastos;
-                    getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, listaGastosFragment).commit();
+                    getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, listaGastosFragment).addToBackStack(null).commit();
                     return true;
                 }
                 return false;
@@ -65,4 +71,18 @@ public class ListasGastosDuedasFragment extends Fragment {
         inflater.inflate(R.menu.menu_listas_gastos_deudas, menu);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (currentFragment == R.layout.fragment_deudas_list) {
+            deudasListFragmentragment = new DeudasListFragment();
+            getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, deudasListFragmentragment).commit();
+        } else {
+            if (bottomNavigationView != null) {
+                bottomNavigationView.setSelectedItemId(R.id.menu_gastos);
+            }
+            listaGastosFragment = new ListaGastosFragment();
+            getParentFragmentManager().beginTransaction().replace(R.id.fragmentListas, listaGastosFragment).commit();
+        }
+    }
 }
