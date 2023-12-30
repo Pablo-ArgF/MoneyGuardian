3bff09e89c692f881eeea7965d35b4bff303e23a
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-12-30 02:14:59 +0100

    Starting to remake gastos/ingresos view

--- app/src/main/java/com/moneyguardian/FormularioGastoActivity.java
+++ app/src/main/java/com/moneyguardian/FormularioGastoActivity.java
@@ -7,9 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,7 +21,9 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.flexbox.FlexboxLayout;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,6 +43,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class FormularioGastoActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
@@ -46,6 +53,9 @@ public class FormularioGastoActivity extends AppCompatActivity {
     // Formulario
     EditText nombre;
     EditText balance;
+    FlexboxLayout categoryLayout;
+    private String selectedCategory;
+    private View selectedCategoryView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +69,7 @@ public class FormularioGastoActivity extends AppCompatActivity {
 
         TextView textViewNombre = findViewById(R.id.textViewNombreGasto);
         EditText editTextNombre = findViewById(R.id.nombreGastoNuevo);
+        categoryLayout = findViewById(R.id.categoryLayout);
         if (!isIngreso) {
             // Si es un gasto
             textViewNombre.setText(R.string.nombreGastoNuevo);
@@ -94,6 +105,9 @@ public class FormularioGastoActivity extends AppCompatActivity {
             }
         });
 
+
+
+
         // Manejo de la fecha y el calendario
 
         EditText fechaText = findViewById(R.id.editDateGasto);
@@ -121,6 +135,28 @@ public class FormularioGastoActivity extends AppCompatActivity {
         } else {
             query = db.collection("categorias/").whereEqualTo("tipo", "gasto");
         }
+
+
+        View.OnClickListener categoryListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = ((TextView)v.findViewById(R.id.name_category));
+                selectedCategory = textView.getText().toString();
+                
+                //we deselect previous category
+                if(selectedCategoryView != null){
+                    ((TextView)selectedCategoryView.findViewById(R.id.name_category)).setTextColor(getColor(R.color.black));
+                    selectedCategoryView.findViewById(R.id.image_category)
+                            .setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.black)));
+                }
+                //we select this one
+                selectedCategoryView = v;
+                int selectedColor = getColor(R.color.blue);
+                ((TextView)selectedCategoryView.findViewById(R.id.name_category)).setTextColor(selectedColor);
+                selectedCategoryView.findViewById(R.id.image_category)
+                        .setBackgroundTintList(ColorStateList.valueOf(selectedColor));
+            }
+        };
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -129,20 +165,28 @@ public class FormularioGastoActivity extends AppCompatActivity {
                 refs.forEach(ref -> {
                     categorias.add((String) ref.get("nombre"));
                 });
-                spCategorias.setAdapter(new ArrayAdapter<String>(getBaseContext(),
-                        android.R.layout.simple_spinner_item, categorias));
-            }
-        });
+                /*spCategorias.setAdapter(new ArrayAdapter<String>(getBaseContext(),
+                        android.R.layout.simple_spinner_item, categorias));*/
+                //we add the cards to the categoryLayout
+                for(String category : categorias){
+                    // Inflate the LinearLayout from an XML layout file
+                    View view = LayoutInflater.from(FormularioGastoActivity.this)
+                            .inflate(R.layout.category_card,
+                            categoryLayout, false);
+                    //We add the click listener to the view
+                    view.setOnClickListener(categoryListener);
+                    // Get the TextView from the inflated layout
+                    TextView textView = view.findViewById(R.id.name_category);
+                    // Set the category name as the text of the TextView
+                    textView.setText(category);
+                    //set the image of the category as the icon on the card
+                    view.findViewById(R.id.image_category).setBackgroundDrawable(
+                            getDrawable(GastosUtil.getImageFor(category)));
+                    //We add the view to the layout
+                    categoryLayout.addView(view);
 
+                }
 
-        spCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                modoOscuro(parent);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
     }
@@ -182,26 +226,11 @@ public class FormularioGastoActivity extends AppCompatActivity {
             balance.setError(getString(R.string.error_empty_balance));
             return false;
         }
-        return true;
-    }
-
-    /**
-     * Este metodo no rebería realmente usarse, ya que la interfaz solo
-     * debería depender de los themes
-     */
-    private void modoOscuro(AdapterView<?> parent) {
-        int nightModeFlags =
-                getResources().getConfiguration().uiMode &
-                        Configuration.UI_MODE_NIGHT_MASK;
-        switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_YES:
-                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
-                break;
-
-            default:
-                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
-                break;
+        if(selectedCategory == null) {
+            Toast.makeText(this,getText(R.string.error_no_category),Toast.LENGTH_LONG).show();
+            return false;
         }
+        return true;
     }
 
 }
